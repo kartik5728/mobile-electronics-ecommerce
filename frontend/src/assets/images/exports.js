63c0fa4c4582d2c16032d53ca1bc35e052eeefc5
Kartik <kartikshivankar5728@gmail.com>
2026-01-09 11:28:29 +0530

    Created Top Navigation For Desktop

--- frontend/src/assets/images/exports.js
+++ frontend/src/assets/images/exports.js
@@ -1,7 +1,10 @@
 import logo_1 from './logo.png'
 import logo_2 from './logo_2.png'
 import logo_3 from './logo_3.png'
+import logo_4 from './logo_4.png'
+import discount_1 from './discount.png'
+import discount_2 from './discount_2.png'
 
 export const frontend_assets = { 
-    logo_1, logo_2, logo_3,
+    logo_1, logo_2, logo_3, logo_4, discount_1, discount_2
 }
